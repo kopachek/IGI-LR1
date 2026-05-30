816264e00fc77dfa4ae4fc8a040eb0b679d6156d
kopachek <kopachekpat@gmail.com>
2026-05-30 22:52:24 +0300

    Fix server time

--- IGI/LR5/shop/context_processors.py
+++ IGI/LR5/shop/context_processors.py
@@ -11,29 +11,15 @@ from shop.utils import format_datetime_dd_mm_yyyy, text_calendar
 
 
 def site_context(request):
-    """Inject user timezone, current dates, and FX rate."""
-    tz_name = request.session.get('django_timezone', 'Europe/Minsk')
-    try:
-        user_tz = zoneinfo.ZoneInfo(tz_name)
-    except zoneinfo.ZoneInfoNotFoundError:
-        user_tz = zoneinfo.ZoneInfo('Europe/Minsk')
+    """Inject timezone, current dates, and FX rate."""
 
-    now_utc = timezone.now()
-    now_local = timezone.localtime(now_utc, user_tz)
-
-    server_tz = zoneinfo.ZoneInfo(datetime.now().astimezone().tzname())
-    server_now = timezone.localtime(now_utc)
-
-
+    server_now = datetime.now().astimezone()
+    server_tz = server_now.tzinfo
     fx = fetch_exchange_rate('BYN', 'USD')
 
     return {
-        'user_timezone': tz_name,
-        'current_date_local': now_local.strftime('%d/%m/%Y'),
-        'current_datetime_local': format_datetime_dd_mm_yyyy(now_utc, user_tz),
-        'current_datetime_utc': format_datetime_dd_mm_yyyy(now_utc, dt_timezone.utc),
         'server_tz': server_tz,
-        'server_datetime': format_datetime_dd_mm_yyyy(now_utc, server_tz),
-        'text_calendar': text_calendar(now_local.year, now_local.month),
+        'server_datetime': format_datetime_dd_mm_yyyy(server_now, server_tz),
+        'text_calendar': text_calendar(server_now.year, server_now.month),
         'fx_rate': fx.get('rate'),
     }
